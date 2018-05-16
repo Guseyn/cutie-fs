dee889e35c4c46ccbe4b399a62db808211aa83bc
gismayilov <gismayilov@griddynamics.com>
2018-05-16 11:20:03 +0300

    in progress

---
+++ test/file/CopiedFileSyncTest.js
@@ -0,0 +1,24 @@
+'use strict'
+
+const {
+  Assertion
+} = require('@guseyn/cutie-assert');
+const {
+  CopiedFileSync,
+  StatsByPath,
+  IsFile,
+  UnlinkedFile
+} = require('./../../index');
+
+const src = './test/file/files/test-7.txt';
+const dest = './test/file/files/test-7-copy.txt'
+
+new Assertion(
+  new IsFile(
+    new StatsByPath(
+      new CopiedFileSync(src, dest)
+    )
+  )
+).after(
+  new UnlinkedFile(dest)
+).call();
---
+++ test/file/CopiedFileTest.js
@@ -0,0 +1,24 @@
+'use strict'
+
+const {
+  Assertion
+} = require('@guseyn/cutie-assert');
+const {
+  CopiedFile,
+  StatsByPath,
+  IsFile,
+  UnlinkedFile
+} = require('./../../index');
+
+const src = './test/file/files/test-8.txt';
+const dest = './test/file/files/test-8-copy.txt'
+
+new Assertion(
+  new IsFile(
+    new StatsByPath(
+      new CopiedFile(src, dest)
+    )
+  )
+).after(
+  new UnlinkedFile(dest)
+).call();
