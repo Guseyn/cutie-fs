92dc041c65110f2aa86291f7d3578792d72302b2
gismayilov <gismayilov@griddynamics.com>
2018-05-23 10:46:13 +0300

    tests on files

---
+++ test/files/ReadFilesOfDirectoryRecursivelyTest.js
@@ -0,0 +1,47 @@
+'use strict'
+
+const {
+  AsyncObject
+} = require('@guseyn/cutie');
+const {
+  DeepEqualAssertion
+} = require('@guseyn/cutie-assert');
+const {
+  ReadFilesOfDirectoryRecursively
+} = require('./../../index');
+
+const dir = './test/files/dirs/test-dir-3';
+
+// We need to sort files from ReadFilesOfDirectoryRecursively for testing,
+//  because ReadFilesOfDirectoryRecursively is asynchronous
+class SortedFiles extends AsyncObject {
+
+  constructor(files) {
+    super(files);
+  }
+
+  definedSyncCall() {
+    return (files) => {
+      files = files.sort((file1, file2) => {
+        return file1 <= file2 ? -1 : 1;
+      });
+      return files;
+    }
+  }
+
+}
+
+new DeepEqualAssertion(
+  new SortedFiles(
+    new ReadFilesOfDirectoryRecursively(dir)
+  ), 
+  ['test/files/dirs/test-dir-3/test-3-1.txt',
+    'test/files/dirs/test-dir-3/test-dir-3-1/test-3-1-1.txt',
+    'test/files/dirs/test-dir-3/test-dir-3-1/test-dir-3-1-1/test-3-1-1-1.txt',
+    'test/files/dirs/test-dir-3/test-dir-3-1/test-dir-3-1-1/test-3-1-1-2.txt',
+    'test/files/dirs/test-dir-3/test-dir-3-2/test-3-2-1.txt',
+    'test/files/dirs/test-dir-3/test-dir-3-2/test-3-2-2.txt',
+    'test/files/dirs/test-dir-3/test-dir-3-2/test-dir-3-2-1/test-dir-3-2-1-1.txt',
+    'test/files/dirs/test-dir-3/test-dir-3-3/test-3-3-1.txt',
+    'test/files/dirs/test-dir-3/test-dir-3-3/test-3-3-2.txt']
+).call();
---
+++ test/files/ReadFilesOfDirectoryTest.js
@@ -0,0 +1,15 @@
+'use strict'
+
+const {
+  DeepEqualAssertion
+} = require('@guseyn/cutie-assert');
+const {
+  ReadFilesOfDirectory
+} = require('./../../index');
+
+const dir = './test/files/dirs/test-dir-2';
+
+new DeepEqualAssertion(
+  new ReadFilesOfDirectory(dir),
+  [ 'test-2-1.txt', 'test-2-2.txt', 'test-2-3.txt' ]
+).call();
