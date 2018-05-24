e3b9e8b070bd83c65d51b9dda6d906c10b018942
gismayilov <gismayilov@griddynamics.com>
2018-05-24 21:16:28 +0300

    read-stream tests

--- test/read-stream/BytesReadFromReadStreamTest.js
+++ test/read-stream/BytesReadFromReadStreamTest.js
@@ -1,23 +1,42 @@
 'use strict'
 
+const fs = require('fs');
+
+const {
+  as, Event, AsyncObject
+} = require('@guseyn/cutie');
 const {
   EqualAssertion
 } = require('@guseyn/cutie-assert');
+const {
+  BufferFromString,
+  BufferLength
+} = require('@guseyn/cutie-buffer');
 const {
   CreatedReadStream,
   BytesReadFromReadStream,
   StatsByPath,
   IsSocket
 } = require('./../../index');
+const {
+  ReadDataFromReadable
+} = require('@guseyn/cutie-stream');
 
 const file = './test/read-stream/files/test-1.txt';
+const data = 'test buffer';
 
-// TODO: implement cutie-stream and make this test more interesting
-//    Use read call for stream and compare BytesReadFromReadStream with number > 0
-new EqualAssertion(
-  new BytesReadFromReadStream(
+new BufferLength(
+  new BufferFromString(data)
+).as('len').after(
+  new ReadDataFromReadable(
     new CreatedReadStream(
       file
-    )
-  ), 0
+    ).as('rs')
+  ).after(
+    new EqualAssertion(
+      new BytesReadFromReadStream(
+        as('rs')
+      ), as('len')
+    ) 
+  )
 ).call();
