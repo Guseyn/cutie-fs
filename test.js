e283e9345858e830267496a69189178a3b11b50e
guseyn <guseynism@gmail.com>
2019-05-11 09:41:25 +0400

    split tests

--- test.js
+++ test.js
@@ -1,7 +1,17 @@
 'use strict'
 
 const { ExecutedTests } = require('test-executor')
+const testDirs = ['./test/data', './test/file/', './test/files', './test/read-stream', './test/stats', './test/watcher', 'test/write-stream']
 
-new ExecutedTests(
-  './test'
-).call()
+const executedTests = (curIndex = 0, curExecutedTests = new ExecutedTests(testDirs[0]), tree = curExecutedTests) => {
+  if (curIndex < testDirs.length - 1) {
+    curIndex += 1
+    const nextExecutedTests = new ExecutedTests(testDirs[curIndex])
+    curExecutedTests.after(nextExecutedTests)
+    executedTests(curIndex, nextExecutedTests, tree)
+  } else {
+    tree.call()
+  }
+}
+
+executedTests()
